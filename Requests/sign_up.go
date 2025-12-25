cefc8868510f09bb0f422994d80c22f4841fe7ec
marflage <istabraq447@gmail.com>
2025-12-25 19:43:24 +0500

    feat: Add validation for CourseName, ClassSchedule, and ClassSession vis UnmarshalJSON() and change binding source to json for SignUpRequest

--- Requests/sign_up.go
+++ Requests/sign_up.go
@@ -1,23 +1,27 @@
 package requests
 
-// TODO: Is not there a better way to bind data instead of using string tags?
+import (
+	"encoding/json"
+	"fmt"
+)
+
 type SignUpRequest struct {
-	// TODO: Handle validation for API client-sent requests.
-	// TODO: Create a custom validator for whitespace-only strings.
-	FirstName   string     `form:"firstName" binding:"required,noBlank"`
-	LastName    string     `form:"lastName" binding:"required,noBlank"`
-	Email       string     `form:"email" binding:"required,email"`
-	PhoneNumber string     `form:"phoneNumber" binding:"required,numeric,len=11"`
-	CourseName  CourseName `form:"courseName" binding:"required"`
+	FirstName   string     `json:"firstName" binding:"required,noBlank"`
+	LastName    string     `json:"lastName" binding:"required,noBlank"`
+	Email       string     `json:"email" binding:"required,email"`
+	PhoneNumber string     `json:"phoneNumber" binding:"required,numeric,len=11"`
+	CourseName  CourseName `json:"courseName" binding:"required,noBlank"`
 	// TODO: Create different enums for semester number for each course
-	Semester      int           `form:"semester" binding:"required,min=1,max=8"`
-	ClassSchedule ClassSchedule `form:"classSchedule" binding:"required"`
-	ClassSession  ClassSession  `form:"classSession" binding:"required"`
-	Password      string        `form:"password" binding:"required,min=8,noBlank"`
+	Semester      int           `json:"semester" binding:"required,min=1,max=8"`
+	ClassSchedule ClassSchedule `json:"classSchedule" binding:"required,noBlank"`
+	ClassSession  ClassSession  `json:"classSession" binding:"required,noBlank"`
+	Password      string        `json:"password" binding:"required,min=8,noBlank"`
+	// TODO: Add field for location.
 }
 
 type CourseName string
 
+// TODO: Find a way to synchronize reference data in the DB and the values of the enum.
 const (
 	QS  CourseName = "Quranic Sciences (QS)"
 	QHD CourseName = "Quran & Hadeeth Dimensions (QHD)"
@@ -25,15 +29,49 @@ const (
 	DEN CourseName = "Dars e Nizami"
 )
 
+func (courseName *CourseName) UnmarshalJSON(data []byte) error {
+	var s string
+
+	if err := json.Unmarshal(data, &s); err != nil {
+		return err
+	}
+
+	switch CourseName(s) {
+	case QS, QHD, QR, DEN:
+		*courseName = CourseName(s)
+		return nil
+	default:
+		return fmt.Errorf("Invalid course name: %s", s)
+	}
+}
+
 type ClassSchedule string
 
+// TODO: Find a way to synchronize reference data in the DB and the values of the enum.
 const (
 	Weekend ClassSchedule = "Weekend"
-	weekday ClassSchedule = "Weekday"
+	Weekday ClassSchedule = "Weekday"
 )
+
+func (classSchedule *ClassSchedule) UnmarshalJSON(data []byte) error {
+	var s string
+
+	if err := json.Unmarshal(data, &s); err != nil {
+		return err
+	}
+
+	switch ClassSchedule(s) {
+	case Weekday, Weekend:
+		*classSchedule = ClassSchedule(s)
+		return nil
+	default:
+		return fmt.Errorf("Invalid class schedule: %s", s)
+	}
+}
 
 type ClassSession string
 
+// TODO: Find a way to synchronize reference data in the DB and the values of the enum.
 // TODO: Include time along with session name
 const (
 	Morning   ClassSession = "Morning"
@@ -41,4 +79,18 @@ const (
 	Evening   ClassSession = "Evening"
 )
 
-// TODO: Create IsValid and Un/Marshal methods for all the enums
+func (classSession *ClassSession) UnmarshalJSON(data []byte) error {
+	var s string
+
+	if err := json.Unmarshal(data, &s); err != nil {
+		return err
+	}
+
+	switch ClassSession(s) {
+	case Morning, Afternoon, Evening:
+		*classSession = ClassSession(s)
+		return nil
+	default:
+		return fmt.Errorf("Invalid class session: %s", s)
+	}
+}
